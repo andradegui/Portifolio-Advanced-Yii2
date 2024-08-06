c1483be07813441808ecb6e391de43e3c9e0b538
Guilherme Rafael Andrade Lima <guilima102013@gmail.com>
2024-08-06 09:47:02 -0300

    Configurando nome da aplicação

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'Portifólio Yii2',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
