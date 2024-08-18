d639f1b0b4ea8ab1799ffb18fbf9b0a6f6ac1071
Guilherme Rafael Andrade Lima <guilima102013@gmail.com>
2024-08-18 20:03:52 -0300

    Criando nova tela de view de projetos (frontend)

--- frontend/views/project/view.php
+++ frontend/views/project/view.php
@@ -11,20 +11,24 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Projects'), 'url' =>
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
+
 <div class="project-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'tech_stack:ntext',
-            'description:ntext',
-            'start_date',
-            'end_date',
-        ],
-    ]) ?>
+   <div class="project-view__dates">
+    <span class="fw-bold d-block"><?= Yii::t('app', 'Date:'); ?></span>
+    <?= $model->start_date . ' ' . Yii::t('app', 'to') . ' ' . $model->end_date; ?>
+   </div>
+
+   <div class="project_view__tech-stack">
+    <span class="fw-bold"><?= Yii::t('app', 'Tech stack:'); ?></span>
+    <?= $model->tech_stack ?>
+   </div>
+
+   <div class="project-view__description d-block">
+    <span class="fw-bold"><?= Yii::t('app', 'Description:'); ?></span>
+    <?= $model->description ?>
+   </div>
 
 </div>
